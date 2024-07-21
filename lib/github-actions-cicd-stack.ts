204f09c27dc7cd0d017033434701030e1c11f9ed
Hai Xu <xuhai_cn@163.com>
2024-07-21 23:00:54 +0800

    Initial commit

---
+++ lib/github-actions-cicd-stack.ts
@@ -0,0 +1,48 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as dotenv from "dotenv";
+
+export class GithubActionsCicdStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Load the environment .env file.
+    dotenv.config();
+
+    // Create a table to store some data.
+    const table = new dynamodb.Table(this, "GithubActionsCicd_VisitorTimeTable", {
+      partitionKey: {
+        name: "key",
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    const lambdaFunction = new lambda.Function(this, "GithubActionsCicd_LambdaFunction", {
+      runtime: lambda.Runtime.PYTHON_3_11,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "main.handler",
+      environment: {
+        VERSION: process.env.VERSION || "0.0",
+        TABLE_NAME: table.tableName,
+      },
+    });
+
+    table.grantReadWriteData(lambdaFunction);
+
+    const functionUrl = lambdaFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+        allowedMethods: [lambda.HttpMethod.ALL],
+        allowedHeaders: ["*"],
+      },
+    });
+
+    new cdk.CfnOutput(this, "Url", {
+      value: functionUrl.url,
+    });
+  }
+}
